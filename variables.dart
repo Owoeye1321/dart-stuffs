8a55a9b30bcc658198a8646938fc127d7985f6d0
Owoeye Samuel <owoeye1321@gmail.com>
2023-04-08 04:44:06 +0100

    journey started

--- variables.dart
+++ variables.dart
@@ -1,5 +1,6 @@
 void main() {
   String name = "Samuel";
   int age = 21;
-  print("My name is $name and i'm $age years old");
+  print(
+      "My name is $name and i'm $age years old, so nice to start a dart and flutter journey");
 }
